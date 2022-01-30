080aecafb9a52dffa1b604ffca6f978752cf88e8
awujo <71509142+awujo@users.noreply.github.com>
2022-01-31 00:25:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5441,22 +5441,21 @@ function hexToRgb(e){var a=/^#?([a-f\d])([a-f\d])([a-f\d])$/i;e=e.replace(a,func
 			<div class="row">
 				<div class="col-4">
 					<div class="followers-select-item followers-select-item-1 active">
-						<img src="img/f-img-1.png" class="img-fluid f-img">
-						<span class="followers-value followers-value-1">500</span>
+						
+						<span class="followers-value followers-value-1"><checkbox>500</checkbox></span>
 						<span class="followers-label">Followers</span>
 					</div>
 				</div>
 				<div class="col-4">
 					<div class="followers-select-item followers-select-item-2">
-						<img src="img/f-img-2.png" class="img-fluid f-img">
-						<span class="followers-value followers-value-2">1000</span>
+						
+						<span class="followers-value followers-value-2"><checkbox>1000</checkbox></span>
 						<span class="followers-label">Followers</span>
 					</div>
 				</div>
 				<div class="col-4">
 					<div class="followers-select-item followers-select-item-3">
-						<img src="img/f-img-3.png" class="img-fluid f-img">
-						<span class="followers-value followers-value-3">2500</span>
+						<span class="followers-value followers-value-3"><checkbox>2500</checkbox></span>
 						<span class="followers-label">Followers</span>
 					</div>
 				</div>
